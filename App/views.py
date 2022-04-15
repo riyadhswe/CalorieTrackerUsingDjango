5959672e3de682cd0b70dab1e2da2b284a846afd
riyadhswe <riyadhswe@gmail.com>
2022-04-15 21:53:34 +0600

    create index update url and create view

--- App/views.py
+++ App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    foods = Food.objects.all()
+
+    contex = {
+        'foods' : foods
+    }
+
+    return render(request,'index.html',contex)
