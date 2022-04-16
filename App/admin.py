88084ed7051c56bba8350b8a7002fc20c1bdceb9
riyadhswe <riyadhswe@gmail.com>
2022-04-16 23:41:20 +0600

    all Code

--- App/admin.py
+++ App/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Food,Consume
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Consume)
--- App/views.py
+++ App/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect
+from .models import Food, Consume
 
 
 # Create your views here.
 def index(request):
-    foods = Food.objects.all()
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
 
-    contex = {
-        'foods' : foods
-    }
 
-    return render(request,'index.html',contex)
+    else:
+        foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+
+    return render(request, 'index.html', {'foods': foods, 'consumed_food': consumed_food})
+
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
